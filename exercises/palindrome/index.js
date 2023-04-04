7582721f590c8935b466c3930dd0835a5622e03c
bryan <obraye05@gmail.com>
2023-04-04 10:52:34 +0100

    ReversedString

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => char === str[str.length - i - 1]);
+}
 
 module.exports = palindrome;
+
+// FIRST SOLUTION
+// function palindrome(str) {
+//   reversed = str.split('').reverse().join('');
+//   return str  === reversed;
+// }
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const arr = str.split('');
+  return arr.reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+// FIRST SOLUTION USING THE 'REVERSE HELPER METHOD
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// SOLUTION 2 USING A FOR LOOP
+// function reverse(str) {
+//   reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
