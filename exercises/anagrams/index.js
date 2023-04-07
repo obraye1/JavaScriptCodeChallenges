ea8f060df6aaf9c86de896f9897b91bb69d74147
bryan <obraye05@gmail.com>
2023-04-07 06:05:48 +0100

    4 more

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
-
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 module.exports = anagrams;
+
+// BUILDING A OBJECT MAP TO SOLVE IT
+
+// function anagrams(stringA, stringB) {
+//   const aCleaned = charBuilder(stringA);
+//   const bCleaned = charBuilder(stringB);
+
+//   if (Object.keys(aCleaned).length !== Object.keys(bCleaned).length) {
+//     return false;
+//   }
+//   for (let char in aCleaned) {
+//     if (aCleaned[char] !== bCleaned[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// function charBuilder(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
